f829083ccfc1adc49bb55a132958a147f6690f70
mahamoda khaton <mhmdkhton@gmail.com>
2022-11-18 00:40:02 -0800

    hw

---
+++ src/githubDemo/HelloGitHub.java
@@ -0,0 +1,8 @@
+package githubDemo;
+public class HelloGitHub {
+	public static void main(String[] args) {
+		System.out.println(
+				"i know how to creat a separate remote repository in Github, and can push my project GithubDemo without help of anyone.");
+	}
+
+}
